2063833a69f891a6f9dc53104207d4ecc6633b29
mhartington <mikehartington@gmail.com>
2015-02-16 19:39:55 -0500

    fix(sessionCtrl): removed old code)

--- www/js/app.js
+++ www/js/app.js
@@ -97,38 +97,4 @@ angular.module('starter', ['ionic', 'ngCordova'])
       saveToPhotoAlbum: false
     });
   };
-
-
-
-
-})
-
-.controller('MainCtrl', function($scope, $cordovaCamera, $cordovaSocialSharing) {
-  var photo = $scope.lastPhoto;
-
-  $scope.getPhoto = function() {
-
-    $cordovaCamera.getPicture().then(function(imageURI) {
-
-      console.log(imageURI);
-
-      photo = imageURI;
-
-      $cordovaSocialSharing
-        .shareViaTwitter("", photo)
-        .then(function(result) { /* Success! */ },
-          function(err) {
-            alert("Ruh-roh");
-          });
-
-
-    }, function(err) {
-      console.err(err);
-    }, {
-      quality: 75,
-      targetWidth: 320,
-      targetHeight: 320,
-      saveToPhotoAlbum: false
-    });
-  };
-})
+});
